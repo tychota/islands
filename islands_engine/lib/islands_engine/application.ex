8db966421eae8b4512f3a7882928898924b1a980
tychota <tychota@users.noreply.github.com>
2017-12-10 16:27:42 +0100

    Add islands project

---
+++ islands_engine/lib/islands_engine/application.ex
@@ -0,0 +1,19 @@
+defmodule IslandsEngine.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      {Registry, keys: :unique, name: Registry.Game},
+      IslandsEngine.GameSupervisor
+    ]
+
+    :ets.new(:game_state, [:public, :named_table])
+
+    opts = [strategy: :one_for_one, name: IslandsEngine.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
